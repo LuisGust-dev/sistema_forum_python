7aa13e5210631b1c40d44edf890e93642678dbc6
LuisGust-dev <dev.gulcs@gmail.com>
2025-06-20 12:05:29 -0300

    Configurando Componentes de Mensagem de erro em BootsTrap

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,8 @@ import sys
 import os
 from dotenv import load_dotenv 
 from corsheaders.defaults import default_headers
-
+# Configuração de Messages (Class Boostrap) #
+from django.contrib.messages import constants
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -86,7 +87,7 @@ THIRD_APPS = [
 
 PROJECT_APPS = [ 
          'apps.base',		
-        # 'apps.myapp',   
+        'apps.pages',   
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_APPS + PROJECT_APPS
@@ -109,13 +110,15 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'base.context_processors.context_social', 
             ],
         },
     },
@@ -233,3 +236,14 @@ EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') 
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
+
+
+
+
+MESSAGE_TAGS = {
+	constants.ERROR: 'alert-danger',
+	constants.WARNING: 'alert-warning',
+	constants.DEBUG: 'alert-danger',
+	constants.SUCCESS: 'alert-success',
+	constants.INFO: 'alert-info',
+}
